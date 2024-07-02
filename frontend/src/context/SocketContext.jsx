814dfa7c4fb4ab0c9c60128a932909d3d9a9c67f
Saurabhn16 <120300109+Saurabhn16@users.noreply.github.com>
2024-07-02 22:02:00 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("/socket.io", {
+      const socket = io("/https://startuponboard.onrender.com", {
         query: {
           userId: authUser._id,
         },
